5bb97b136a8a134a67cba7d5a18648bc060ff631
Stefano Napolitano <stefano.napo94@gmail.com>
2022-04-16 19:43:46 +0200

    Task Checker v1

---
+++ src/main/java/com/mycompany/taskchecker/TaskChecker.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.taskchecker;
+
+/**
+ *  This is a simple app to write daily task up to 10 at the same time and
+ * it can be edited 1 by 1 or deleted and "cleared" all of them with the press of a button.
+ *  This simple proyect is just for testing jFrames, jLabels and jButtons capabilities.
+ * And also to get a grasp on the NetBeans IDE.
+ * 
+ * Version 0.1
+ * 
+ * @author StefanoNapolitano
+ */
+public class TaskChecker {
+
+    public static void main(String[] args) {
+      new MainFrame().setVisible(true);
+    }
+}
